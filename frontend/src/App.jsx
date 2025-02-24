9ce5e960de0ddd9e2d0a12f6a082e78dd364e8ea
DoMo <oudompov085@gmail.com>
2025-02-24 18:23:39 +0700

    create vite with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>Let's build something awesome.</p>
+    </div>
+  );
+}
+
+export default App;
